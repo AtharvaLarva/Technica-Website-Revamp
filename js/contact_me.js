ae40b601717ef2115a52af6a17e40c63b5c3f846
Atharva Bhat <abhat98@gmail.com>
2017-06-21 21:42:23 -0400

    Removed button border, updated FAQs, and configured contact form with formspree

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,7 +12,6 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -20,11 +19,11 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                /* using formspree to send forms from contact me page */
+                url: 'https://formspree.io/abhat98@gmail.com',
+                type: 'POST',
                 data: {
                     name: name,
-                    phone: phone,
                     email: email,
                     message: message
                 },
